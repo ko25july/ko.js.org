5f63cb5260b884873b1b5a22cc43475cf931053f
KO <ko25july@gmail.com>
2018-03-18 14:32:55 +0700

    Update ws-node.js

--- ws-node/ws-node.js
+++ ws-node/ws-node.js
@@ -1,4 +1,4 @@
-exports = function() {
+module.exports = function() {
   console.log("This is a message from the demo package");
   const HOST_HEROKU = "ws-node.herokuapp.com";
   const URL_KO_JS_ORG = "https://ko.js.org";
