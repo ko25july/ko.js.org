af9ee77e9a14fd666d772275b395a96455cd4513
KO <ko25july@gmail.com>
2018-03-20 14:29:47 +0700

    Update ESP_7055E7.js

--- ws-node/devices/ESP_7055E7.js
+++ ws-node/devices/ESP_7055E7.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   var wsHost = initJSON.INIT.slice(initJSON.INIT.indexOf("://") + 3, initJSON.INIT.indexOf("/", initJSON.INIT.indexOf("://") + 3));
   var wsOrigin = initJSON.INIT.slice(0, initJSON.INIT.indexOf("/", initJSON.INIT.indexOf("://") + 3));
   var wsClient = new require("ws")(wsHost, {
@@ -8,29 +8,25 @@
     }
   });
 
-  wsClient.on("open", function() {
+  wsClient.on("open", function () {
     this.send(JSON.stringify({
-      "device": initJSON.HOST_NAME
+      "device": "ONLINE"
     }));
   });
 
-  wsClient.on("message", function(message) {
+  wsClient.on("message", function (message) {
     var msgJSON;
 
     try {
       msgJSON = JSON.parse(message);
 
-      if (msgJSON.device === initJSON.HOST_NAME) {
-        msgJSON.message = eval(msgJSON.message);
-        this.send(JSON.stringify(msgJSON));
+      if (msgJSON.device && msgJSON.device !== "ONLINE") {
+        this.send(JSON.stringify({ "message": eval(msgJSON.device) }));
       }
     } catch (error) {
-      this.send(JSON.stringify({
-        "device": initJSON.HOST_NAME,
-        "message": error.toString()
-      }));
-    } finally {
-      delete msgJSON;
+      this.send(JSON.stringify({ "error": error.message }));
     }
+
+    delete msgJSON;
   });
 })();
